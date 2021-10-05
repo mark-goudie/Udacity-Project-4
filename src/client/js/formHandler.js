33555c90c022f419bcdfbb6c9980a9c9700b6f70
Mark Goudie <goudie.mark@gmail.com>
2021-10-05 21:49:22 +1100

    Framework complete

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,48 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+  // check what text was put into the form field
+  let formText = document.getElementById("name").value;
+  //     checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+  //     console.log("::: Form Submitted :::")
+  //     fetch('http://localhost:8080/test')
+  //     .then(res => res.json())
+  //     .then(function(res) {
+  //         document.getElementById('results').innerHTML = res.message
+  //     })
+  // }
+
+  // export { handleSubmit }
+
+  const data = {
+    formText,
+  };
+  if (Client.checkForName(formText)) {
+    fetch("http://localhost:8080/clientInput", {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
+      .then((res) => res.json())
+      .then(function (res) {
+        document.getElementById("model").innerHTML = "Model: " + res.model;
+        document.getElementById("score_tag").innerHTML =
+          "Score: " + res.score_tag;
+        document.getElementById("agreement").innerHTML =
+          "Agreement: " + res.agreement;
+        document.getElementById("subjectivity").innerHTML =
+          "Subjectivity: " + res.subjectivity;
+        document.getElementById("confidence").innerHTML =
+          "Confidence: " + res.confidence;
+        document.getElementById("irony").innerHTML = "Irony: " + res.irony;
+      });
+  } else {
+    alert("Please enter a valid URL");
+  }
 }
 
-export { handleSubmit }
+export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,46 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require("dotenv");
+dotenv.config();
+const key = process.env.API_KEY;
 
-const app = express()
+var path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+const fetch = require("node-fetch");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(express.static('dist'))
+const cors = require("cors");
+app.use(cors());
+app.use(express.static("dist"));
 
-console.log(__dirname)
+console.log(__dirname);
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+app.get("/", function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve("dist/index.html"));
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+  console.log("Example app listening on port 8080!");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
+
+app.post("/clientInput", async (req, res) => {
+  const clientUrl = req.body.url;
+  const urlOutput = await fetch(
+    `https://api.meaningcloud.com/sentiment-2.1?key=${key}&url=${clientURL}&lang=en`
+  );
+  try {
+    const data = await urlOutput.json();
+    console.log(data);
+    res.send(data);
+  } catch (error) {
+    console.log("error", error);
+  }
+});
